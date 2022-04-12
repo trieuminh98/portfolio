76f32fd79c293240c816dd31b79e24b70a2f8410
hoangdangkhoam9 <hoangdangkhoa.m9@gmail.com>
2022-04-12 15:33:44 +0700

    Add typescript + update lastest material UI

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { createStyles, Grid, makeStyles, ThemeProvider } from '@mui/material';
+import './App.css';
+import theme from './theme';
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <div>Helloxcxcxcxcxc</div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/theme.tsx
@@ -0,0 +1,31 @@
+import { createTheme } from '@mui/material/styles';
+import { green, purple } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+    background: {
+      default: '#FAFDD6',
+    },
+  },
+  components: { 
+    // Name of the component
+    // MuiTypography: {
+    //   styleOverrides: {
+    //     // Name of the slot
+    //     root: {
+    //       // Some CSS
+    //       fontSize: '0.8rem',
+    //       color: 'inherit'
+    //     },
+    //   },
+    // },
+  },
+});
+
+export default theme;
